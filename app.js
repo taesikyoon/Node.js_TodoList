10adb29dfd2f628adcfc2709cf0451dd3495487f
sumin-dev <bonar8888@naver.com>
2022-08-10 01:08:33 +0900

    restore

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ const router = require("./routes");
 
 app.use(express.json());
 
+
+
 app.use("/", express.urlencoded({ extended: false }), router);
 
-app.get("/", (req, res) => {
-  res.send("Hello Todolist!");
+app.get('/', (req, res) => {
+  res.send('Hello Todolist!');
 });
 
 app.listen(POST, () => {
   console.log("Server Running");
 });
+
+// const models = require("./models");
+// models.sequelize.sync().then(() => {
+// console.log(" DB 연결 성공");
+// }).catch(err => {
+// console.log("연결 실패");
+// console.log(err);
+// })
