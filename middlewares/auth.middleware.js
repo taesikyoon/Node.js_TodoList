54a598c5edbe5ffcb26567012c4540fe4b8d632b
sumin-dev <bonar8888@naver.com>
2022-08-11 00:56:30 +0900

    auth미들웨어 에러메세지 수정

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,12 +4,14 @@ require("dotenv").config();
 const { Users } = require("../models");
 const SECRET_KEY = process.env.SECRET_KEY;
 module.exports = async (req, res, next) => {
-  const { authorization } = req.headers;
-  const [Type, tokenValue] = authorization.split(" ");
-  if (Type !== "Bearer") {
-    return res.status(400).json({ message: "로그인이 필요합니다." });
-  }
+
   try {
+    const { authorization } = req.headers;
+    const [Type, tokenValue] = authorization.split(" ");
+    if (Type !== "Bearer") {
+      return res.status(400).json({ message: "로그인이 필요합니다." });
+    }
+
     // 검사시작;
     const { nickname } = jwt.verify(tokenValue, SECRET_KEY);
     const data = await Users.findOne({ where: { nickname } });
@@ -21,7 +23,12 @@ module.exports = async (req, res, next) => {
     // 유효성 검사 에러
     if (e.name === "TokenExpiredError")
       return res.status(419).json({ message: "토큰이 만료되었습니다." });
+
     if (e.name === "JsonWebTokenError")
       return res.status(401).json({ message: "유효하지 않은 토큰입니다." });
+
+
+    res.status(400).send("로그인이 필요합니다.")
   }
+
 };
