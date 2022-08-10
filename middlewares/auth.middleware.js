661a1abbed25ac3f47e5f3092a13e1d41adfffae
Taesik yoon <taesikyoon97@naver.com>
2022-08-11 00:57:52 +0900

    auth Update

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,13 +4,13 @@ require("dotenv").config();
 const { Users } = require("../models");
 const SECRET_KEY = process.env.SECRET_KEY;
 module.exports = async (req, res, next) => {
-  const { authorization } = req.headers;
-  const [Type, tokenValue] = authorization.split(" ");
-  if (Type !== "Bearer") {
-    return res.status(400).json({ message: "로그인이 필요합니다." });
-  }
   try {
     // 검사시작;
+    const { authorization } = req.headers;
+    const [Type, tokenValue] = authorization.split(" ");
+    if (Type !== "Bearer") {
+      return res.status(400).json({ message: "로그인이 필요합니다." });
+    }
     const { nickname } = jwt.verify(tokenValue, SECRET_KEY);
     const data = await Users.findOne({ where: { nickname } });
     if (data === null) res.status(400).json({ errorMessage: "error" });
@@ -23,5 +23,6 @@ module.exports = async (req, res, next) => {
       return res.status(419).json({ message: "토큰이 만료되었습니다." });
     if (e.name === "JsonWebTokenError")
       return res.status(401).json({ message: "유효하지 않은 토큰입니다." });
+    res.status(400).send("로그인이 필요합니다.");
   }
 };
